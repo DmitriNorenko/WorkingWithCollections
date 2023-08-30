e7190ef2d995d53d997152613226444174881a1d
Name <email@exaple.ru>
2023-08-30 09:17:11 +0300

    Final

--- WorkingWithCollections/Program.cs
+++ WorkingWithCollections/Program.cs
@@ -1,12 +1,19 @@
-﻿namespace WorkingWithCollections
+﻿using System.Diagnostics;
+
+namespace WorkingWithCollections
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var stopWatch1 = Stopwatch.StartNew();
             List.ReadList();
+            Console.WriteLine(stopWatch1.Elapsed.TotalMilliseconds);
             Console.ReadKey();
+            var stopWatch2 = Stopwatch.StartNew();
             LinkedList.ReadLinkedList();
+            Console.WriteLine(stopWatch2.Elapsed.TotalMilliseconds);
+            Console.ReadKey();
         }
     }
 }
